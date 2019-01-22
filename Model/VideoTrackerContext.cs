87235875fed64c080f0fb89c39bc0b864f78d651
AjlaniWael <ajlani.wael@gmail.com>
2019-01-22 13:47:33 +0100

    Initial commit

---
+++ Model/VideoTrackerContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VideoTracker.Model
+{
+    public class VideoTrackerContext : DbContext
+    {
+        public VideoTrackerContext(DbContextOptions<VideoTrackerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Video> Videos { get; set; }
+    }
+}
